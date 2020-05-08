034f338a5fc7605ec82c6ee8484c93d3766f2958
wasaby5423 <55898035+wasaby5423@users.noreply.github.com>
2020-05-09 02:39:47 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,42 +22,42 @@ def Main():
 			print(logo)
 			print(info)
 			if token == "":
-				print(Fore.GREEN+"Введите полученный токен:"+Style.RESET_ALL)
+				print(Fore.GREEN+"token:"+Style.RESET_ALL)
 				token = input(Fore.BLUE+"VK-Covid-Status > "+Style.RESET_ALL)
-				print(Fore.GREEN+"Сохранить данный токен для дальнейшего использования? \n"+Style.RESET_ALL+"(Токен хранится у Вас в файле covid.py и никуда не передается)\n"+Fore.GREEN+"(y/n)"+Style.RESET_ALL)
+				print(Fore.GREEN+"\n"+Style.RESET_ALL+"\n"+Fore.GREEN+"(y/n)"+Style.RESET_ALL)
 				choice = input(Fore.BLUE+"VK-Covid-Status > "+Style.RESET_ALL)
 				if choice == "y":
 					os.system("echo token=\"'{}'\">~/vk-covid/covid.py".format(token))
-					print(Fore.GREEN+"Токен сохранен"+Style.RESET_ALL)
+					print(Fore.GREEN+""+Style.RESET_ALL)
 				else:
-					print(Fore.RED+"Токен не сохранен"+Style.RESET_ALL)
+					print(Fore.RED+""+Style.RESET_ALL)
 			else:
-				print(Fore.GREEN+"Выгрузить сохраненный токен? (y/n)"+Style.RESET_ALL)
+				print(Fore.GREEN+" (y/n)"+Style.RESET_ALL)
 				choice = input(Fore.BLUE+"VK-Covid-Status > "+Style.RESET_ALL)
 				if choice == "y":
 					pass
 				elif choice == "n":
-					print(Fore.GREEN+"Введите полученный токен:"+Style.RESET_ALL)
+					print(Fore.GREEN+""+Style.RESET_ALL)
 					token = input(Fore.BLUE+"VK-Covid-Status > "+Style.RESET_ALL)
 					if token == "":
-						print(Fore.RED+"Неверное значение"+Style.RESET_ALL)
+						print(Fore.RED+""+Style.RESET_ALL)
 						time.sleep(3)
 						Main()
-					print(Fore.GREEN+"Сохранить новый токен для дальнейшего использования? \n"+Style.RESET_ALL+"(Токен хранится у Вас в файле covid.py и никуда не передается)\n"+Fore.GREEN+"(y/n)"+Style.RESET_ALL)
+					print(Fore.GREEN+"? \n"+Style.RESET_ALL+"(Токен хранится у Вас в файле covid.py и никуда не передается)\n"+Fore.GREEN+"(y/n)"+Style.RESET_ALL)
 					choice = input(Fore.BLUE+"VK-Covid-Status > "+Style.RESET_ALL)
 					if choice == "y":
 						os.system("echo token=\"'{}'\">~/vk-covid/covid.py".format(token))
-						print(Fore.GREEN+"Токен сохранен"+Style.RESET_ALL)
+						print(Fore.GREEN+""+Style.RESET_ALL)
 					else:
-						print(Fore.RED+"Токен не сохранен"+Style.RESET_ALL)
+						print(Fore.RED+""+Style.RESET_ALL)
 				else:
-					print(Fore.RED+"Неверное значение"+Style.RESET_ALL)
+					print(Fore.RED+""+Style.RESET_ALL)
 					Main()
-			print(Fore.GREEN+"Введите период обновления статуса:"+Style.RESET_ALL)
-			print(Fore.GREEN+"Пример:\ns30 - 30 секунд\nm5 - 5 минут\nh1 - 1 час\nd1 - 1 день."+Style.RESET_ALL)
+			print(Fore.GREEN+":"+Style.RESET_ALL)
+			print(Fore.GREEN+"\ns30 - 30 \nm5 - 5 \nh1 - 1 \nd1 - 1 ."+Style.RESET_ALL)
 			uptime = input(Fore.BLUE+"VK-Covid-Status > "+Style.RESET_ALL)
 			if uptime=="":
-				print(Fore.RED+"Неверное значение"+Style.RESET_ALL)
+				print(Fore.RED+""+Style.RESET_ALL)
 				Main()
 			elif uptime[0]=="s":
 				t=int(uptime[1:])
@@ -68,7 +68,7 @@ def Main():
 			elif uptime[0]=="d":
 				t=int(uptime[1:])*86400
 			clear()
-			info = "Токен: {}\nПериод изменения:{} sec\n\nДля отмены нажмите Ctrl+Z".format(token, t)
+			info = "Токен: {}\n:{} sec\n\n Ctrl+Z".format(token, t)
 			print(logo)
 			print(info)
 			status_id=1
